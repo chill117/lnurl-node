c132e7b74c5af2342abe4d4b51c11b23946e8ef0
Charles Hill <chill@degreesofzero.com>
2020-04-08 18:27:41 +0200

    Fix HTTPError constructor class

--- server/HttpError.js
+++ server/HttpError.js
@@ -1,10 +1,16 @@
 const HttpError = function(message, status) {
-	this.name = 'HttpError';
+	if (!Error.captureStackTrace) {
+		this.stack = (new Error()).stack;
+	} else {
+		Error.captureStackTrace(this, this.constructor);
+	}
 	this.message = message;
 	this.status = status;
 	this.stack = (new Error()).stack;
 };
 
 HttpError.prototype = new Error;
+HttpError.name = 'HttpError';
+HttpError.constructor = HttpError;
 
 module.exports = HttpError;
