b7eeac4a2a5ab8e68f0381fd2eaed706f9667b80
Charles Hill <chill@degreesofzero.com>
2020-10-13 13:17:20 +0200

    Combined 03 and 04 migrations

--- server/stores/migrations/0003_urls_remainingUses.js
+++ server/stores/migrations/0003_urls_remainingUses.js
@@ -1,3 +1,12 @@
+const _ = require('underscore');
+
+const filterResults = function(results) {
+	return _.chain(results).filter(function(result) {
+		const data = _.isString(result.data) ? JSON.parse(result.data) : result.data;
+		return data && data.used === false;
+	}).pluck('hash').value();
+};
+
 exports.up = function(knex) {
 	return knex.schema.hasColumn('urls', 'remainingUses').then(exists => {
 		if (!exists) {
@@ -5,6 +14,13 @@ exports.up = function(knex) {
 				table.integer('remainingUses').unsigned().defaultTo(0);
 			});
 		}
+	}).then(() => {
+		return knex('urls').select('*').then(results => {
+			const hashes = filterResults(results);
+			return knex('urls')
+				.update({ remainingUses: 1 })
+				.whereIn('hash', hashes);
+		});
 	});
 };
 
--- test/unit/global-hooks.js
+++ test/unit/global-hooks.js
@@ -13,6 +13,20 @@ before(function(done) {
 	fs.mkdir(this.tmpDir, { recursive: true }, done);
 });
 
+if (process.env.LNURL_STORE_BACKEND === 'knex') {
+	const Store = require('../../server/stores/knex');
+	after(function() {
+		const config = process.env.LNURL_STORE_CONFIG && JSON.parse(process.env.LNURL_STORE_CONFIG) || {};
+		const store = new Store(config);
+		return store.onReady().then(() => {
+			// Rollback all migrations.
+			return store.db.migrate.rollback(null, true).then(() => {
+				return store.close();
+			});
+		});
+	});
+}
+
 after(function(done) {
 	fs.remove(this.tmpDir, done);
 });
