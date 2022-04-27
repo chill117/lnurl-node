0c2211dd8c43c29eb4d77c2a6ecdb00c3743f674
Charles Hill <chill@degreesofzero.com>
2022-04-27 12:09:45 +0100

    Tests for promiseAllSeries

---
+++ test/unit/lib/promiseAllSeries.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+const { promiseAllSeries } = require('../../../lib');
+
+describe('promiseAllSeries', function() {
+
+	it('executes all promises in series', function() {
+		const values = ['one', 'two', 'three'];
+		let run = [];
+		return promiseAllSeries(values.map((value, index) => {
+			return () => {
+				return Promise.resolve().then(() => {
+					run.push(value);
+					assert.strictEqual(run.length, index + 1);
+					assert.strictEqual(value, values[index]);
+					return new Promise((resolve, reject) => {
+						setTimeout(() => resolve(value), 5);
+					});
+				});
+			};
+		})).then(result => {
+			assert.deepStrictEqual(result, values[values.length - 1]);
+			assert.deepStrictEqual(run, values);
+		});
+	});
+
+	it('does not run promises after a thrown error', function() {
+		const values = ['one', 'two', 'three'];
+		let run = [];
+		return promiseAllSeries(values.map((value, index) => {
+			return () => {
+				return Promise.resolve().then(() => {
+					if (value === 'two') {
+						throw new Error('Thrown inside "two"');
+					}
+					run.push(value);
+				});
+			};
+		})).then(() => {
+			throw new Error('Expected an error');
+		}).catch(error => {
+			assert.strictEqual(error.message, 'Thrown inside "two"');
+		}).finally(() => {
+			assert.deepStrictEqual(run, ['one']);
+		});
+	});
+});
